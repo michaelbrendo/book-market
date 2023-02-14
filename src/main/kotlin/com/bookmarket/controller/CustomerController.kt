22f2737e9a8affbde69751493203a9471d27b60d
Michael Santos <23271123+michaelbrendo@users.noreply.github.com>
2023-02-13 23:51:56 -0300

    Create controller and model

---
+++ src/main/kotlin/com/bookmarket/controller/CustomerController.kt
@@ -0,0 +1,17 @@
+package com.bookmarket.controller
+
+import com.bookmarket.model.CustomerModel
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("customer")
+class CustomerController {
+
+    @GetMapping
+    fun getCustomer(): CustomerModel {
+        return CustomerModel("1", "Michael", "email@emailll.com")
+    }
+
+}
