28089e834863adc9e48824bbf433aa911cfed83c
Michael Santos <23271123+michaelbrendo@users.noreply.github.com>
2023-02-14 01:20:14 -0300

    Implement a delete customer request

--- src/main/kotlin/com/bookmarket/controller/CustomerController.kt
+++ src/main/kotlin/com/bookmarket/controller/CustomerController.kt
@@ -4,6 +4,7 @@ import com.bookmarket.controller.request.PostCustomerRequest
 import com.bookmarket.controller.request.PutCustomerRequest
 import com.bookmarket.model.CustomerModel
 import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
@@ -43,6 +44,12 @@ class CustomerController {
             it.name = customer.name
             it.email = customer.email
         }
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    fun deleteCustomer(@PathVariable id: String) {
+        customers.removeIf { it.id == id }
     }
 
 }
